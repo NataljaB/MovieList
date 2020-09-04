5de72d3d37338a72eab7239f5bd0b1bde0f4e37e
NataljaB <natalja.buljko@gmail.com>
2020-09-04 08:53:16 +0300

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by natalja.buljko on 04/09/2020.
+//  Copyright © 2020 natalja.buljko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
