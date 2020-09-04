2c63bf3ac3820d4e35850cf62b6804f1d7bb2bb6
NataljaB <natalja.buljko@gmail.com>
2020-09-04 11:36:54 +0300

    DetailView

--- MovieList/DetailViewController.swift
+++ MovieList/DetailViewController.swift
@@ -9,21 +9,15 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieYearLabel: UILabel!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
